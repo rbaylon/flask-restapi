3a9c6a03c8c8249334654334c76b24dea17d42d9
RICARDO BAYLON <baylonrr@ph.ibm.com>
2020-09-06 09:14:15 +0800

    move api routes to /api

--- api/rest.py
+++ api/rest.py
@@ -5,5 +5,5 @@ from baseapp import app
 rest_api = Api(app)
 
 # Build api routes
-rest_api.add_resource(RoomList, '/rooms')
-rest_api.add_resource(Room, '/rooms/<room_id>')
+rest_api.add_resource(RoomList, '/api/rooms')
+rest_api.add_resource(Room, '/api/rooms/<room_id>')
